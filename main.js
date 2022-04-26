ebfa3897c77990211a7949873db3b32560206ab6
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-04-26 22:54:39 +0200

    Iteración #3

--- main.js
+++ main.js
@@ -47,3 +47,20 @@ globalBasePrice = 25000;
 car1.finalPrice = car1.basePrice + globalBasePrice;
 car2.finalPrice = car2.basePrice + globalBasePrice;
 console.log(car1.finalPrice, car2.finalPrice);
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+const multiply = alert(10 * 5);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+const divide = alert(10 / 2);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+const module = alert(15 % 9);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+let x2 = y + z;
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+let x3 = y * 5;
